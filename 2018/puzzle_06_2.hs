bc2e4ce8cc0e300dad0fe9743790d9909747be5a
Nico Tromp <nico.tromp@gmail.com>
2018-12-06 12:00:59 +0100

    Day 6 puzzle 2.

---
+++ 2018/puzzle_06_2.hs
@@ -0,0 +1,17 @@
+import Data.List
+
+type ID = Int
+type Coordinate = (Int,Int)
+
+coordinates :: [Coordinate]
+coordinates = [(124,262),(182,343),(79,341),(44,244),(212,64),(42,240),(225,195),(192,325),(192,318),(42,235),(276,196),(181,262),(199,151),(166,214),(49,81),(202,239),(130,167),(166,87),(197,53),(341,346),(235,241),(99,278),(163,184),(85,152),(349,334),(175,308),(147,51),(251,93),(163,123),(151,219),(162,107),(71,58),(249,293),(223,119),(46,176),(214,140),(80,156),(265,153),(92,359),(103,186),(242,104),(272,202),(292,93),(304,55),(115,357),(43,182),(184,282),(352,228),(267,147),(248,271)]
+-- coordinates = [(1,1),(1,6),(8,3),(3,4),(5,5),(8,9)]
+
+manhattenDistance :: Coordinate -> Coordinate -> Int
+manhattenDistance (x,y) (x',y') = abs (x-x') + abs (y-y')
+
+totalDistance :: Coordinate -> [Coordinate] -> Int
+totalDistance c cs = sum $ map (manhattenDistance c) cs
+
+puzzle_06_2 :: Int
+puzzle_06_2 = length [ 1 | x <- [1..500], y <- [1..500], totalDistance (x,y) coordinates < 10000 ]
